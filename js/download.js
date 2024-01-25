0be80a0406d4d931195c97686059a35ccce8a754
Fridey <f4fridey@gmail.com>
2024-01-25 15:43:20 +0100

    got the download image to work 90%!!!
    uninstalled html2canvas and html-to-image, and installed modern-screenshot

--- js/download.js
+++ js/download.js
@@ -13,16 +13,23 @@
 } */
 
 /* ES6 */
-import * as htmlToImage from 'libs/node_modules/html-to-image';
-import { toPng, toJpeg, toBlob, toPixelData, toSvg } from '../libs/node_modules/html-to-image';
+/* import * as htmlToImage from 'html-to-image';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from '../libs/node_modules/html-to-image'; */
 
 /* ES5 */
-var htmlToImage = require('html-to-image');
+/* var htmlToImage = require('html-to-image.js'); */
 
 function convertHtmlToPng()
 {
-    htmlToImage.toPng(document.getElementById('acsbar'))
+    /* htmlToImage.toPng(document.getElementById('acsbar'))
         .then(function (dataUrl) {
     download(dataUrl, 'acsbar.png');
-  });
+  }); */
+
+
+  modernScreenshot.domToPng(document.querySelector('#acsbar')).then(dataUrl => {
+    const link = document.createElement('a')
+    link.download = 'acsbar.png'
+    link.href = dataUrl
+    link.click()});
 }
